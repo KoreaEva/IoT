4d7fda655a72888268e2a8c2e2df68e77dda4a5b
mildock <youngwook@outlook.com>
2016-05-03 12:25:22 +0900

    Add First

---
+++ Samples/EmotionLib/HappyTestClient/Entities/Store.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyTestClient.Entities
+{
+    public class Store
+    {
+        public Store(string storeName, double latiude, double longitude)
+        {
+            this.StoreName = storeName;
+            this.Longitude = longitude;
+            this.Latitude = latiude;
+        }
+
+        public string StoreName { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}
